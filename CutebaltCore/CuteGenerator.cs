32f6e5df18645c48e716f05dcf429d06012c36ef
Terria-K <teuria@proton.me>
2025-07-15 18:27:53 +0800

    Separate SpicaTriscaffoldingPatches to SpicaFixedStarPatches

--- CutebaltCore/CuteGenerator.cs
+++ CutebaltCore/CuteGenerator.cs
@@ -4,7 +4,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using YamlDotNet.Serialization;
 
 namespace CutebaltCore;
 
