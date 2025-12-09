bbad994ae1a7d38c4f3ad43aa3551cdfc36224d2
Terria-K <teuria@proton.me>
2025-12-10 00:02:56 +0800

    TidalBooster now requires Routed cannon to be active

--- StarcourseDock/Core/Ships/Alpherg/Artifacts/TidalBooster.cs
+++ StarcourseDock/Core/Ships/Alpherg/Artifacts/TidalBooster.cs
@@ -7,6 +7,7 @@ namespace Teuria.StarcourseDock;
 
 internal sealed class TidalBooster : Artifact, IRegisterable
 {
+    public bool isRoutedCannonActive;
     public int underdriveCap;
 
     public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
@@ -32,10 +33,16 @@ internal sealed class TidalBooster : Artifact, IRegisterable
     public override void OnTurnStart(State state, Combat combat)
     {
         underdriveCap = 0;
+        isRoutedCannonActive = true;
     }
 
     public override Spr GetSprite()
     {
+        if (!isRoutedCannonActive)
+        {
+            return Sprites.artifacts_TidalBooster_BothInactive.Sprite;
+        }
+
         if (underdriveCap < 2)
         {
             return Sprites.artifacts_TidalBooster.Sprite;
@@ -44,12 +51,28 @@ internal sealed class TidalBooster : Artifact, IRegisterable
         return Sprites.artifacts_TidalBooster_BlueInactive.Sprite;
     }
 
+    public override void OnPlayerPlayCard(int energyCost, Deck deck, Card card, State state, Combat combat, int handPosition, int handCount)
+    {
+        var routedCannon = state.GetArtifactFromColorless<RoutedCannon>();
+
+        if (routedCannon is not null)
+        {
+            isRoutedCannonActive = !routedCannon.disabled;
+        }
+    }
+
     public override void OnEnemyGetHit(State state, Combat combat, Part? part)
     {
+        if (!isRoutedCannonActive)
+        {
+            return;
+        }
+
         if (part is null)
         {
             return;
         }
+
         var piscium = state.GetArtifactFromColorless<Piscium>();
         if (piscium is null)
         {
@@ -59,11 +82,13 @@ internal sealed class TidalBooster : Artifact, IRegisterable
         if (piscium.isRight)
         {
             combat.otherShip.Add(Status.heat, 1);
+            Pulse();
         }
         else if (underdriveCap < 2)
         {
             combat.otherShip.Add(InfradriveStatus.Infradrive.Status, 1);
             underdriveCap += 1;
+            Pulse();
         }
     }
 
